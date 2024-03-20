9557899d1d4b8cbff65971df4d9d752aec9fe8a1
Lizonka <145397454+Pyrenaria@users.noreply.github.com>
2024-03-20 17:24:32 +0300

    Add files via upload

---
+++ coursework/coursework/Pages/Orders.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace coursework.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Orders.xaml
+    /// </summary>
+    public partial class Orders : Page
+    {
+        public Orders()
+        {
+            InitializeComponent();
+        }
+
+        private void fltr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.ClassFrame.frmObj.Navigate(new Options());
+        }
+    }
+}
